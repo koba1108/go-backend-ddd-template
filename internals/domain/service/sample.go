325138ffbfe9e0be65e6a8a5081cbd399df825c0
koba1108 <paseli.koba@gmail.com>
2024-01-31 15:33:09 +0900

    up

--- internals/domain/service/sample.go
+++ internals/domain/service/sample.go
@@ -1,4 +1,10 @@
 package service
 
+import (
+	"context"
+	"github.com/koba1108/go-backend-ddd-template/internals/domain/model"
+)
+
 type SampleService interface {
+	GetSampleWithAuthUser(ctx context.Context, id int) (*model.Sample, *model.AuthUser, error)
 }
